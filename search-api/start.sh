f9411cdcb3ac38f3a65d1514cdb93c4c35792ecd
Lazar Stamenkovic <lazarstamenkovic96@hotmail.com>
2022-01-24 12:17:29 +0100

    fixed start script

--- search-api/start.sh
+++ search-api/start.sh
@@ -5,5 +5,5 @@ export PYTHONPATH=./
 python ./src/wait_for_deps.py
 python ./src/init_db.py
 
-#gunicorn -w 4 -k uvicorn.workers.UvicornWorker src.main:app -b 0.0.0.0:80
-uvicorn src.main:app --port 80 --host 0.0.0.0 --reload
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker src.main:app -b 0.0.0.0:80
+#uvicorn src.main:app --port 80 --host 0.0.0.0 --reload
